2d4c87da97c229c9d3e516536543ed37c03d7dee
1 <1@DESKTOP-IQ5M5FM>
2022-06-22 14:36:36 +0900

    com

--- githrd/src/main/java/com/githrd/www/controller/Board.java
+++ githrd/src/main/java/com/githrd/www/controller/Board.java
@@ -76,7 +76,7 @@ public class Board {
 		mv.setViewName("board/boardWrite");
 		return mv;
 	}
-<<<<<<< HEAD
+
 	
 	// 게시글 등록 요청 처리함수
 	@RequestMapping("/boardWriteProc.blp")
@@ -94,7 +94,5 @@ public class Board {
 		mv.addObject("VIEW", view);
 		return mv;
 	}
-=======
->>>>>>> refs/remotes/origin/master
 
 }
